98349c9528e49fae32163e296ad880794fb89fd9
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2023-02-16 12:09:48 -0500

    Se modifica para acceder bien desde el front
    Se modifica metodos para bien desde el front.

--- src/presentation/controllers/transfer/transfer.controller.ts
+++ src/presentation/controllers/transfer/transfer.controller.ts
@@ -1,56 +1,77 @@
-import { Body, Controller, Get, Param, ParseUUIDPipe, Post, UseGuards } from "@nestjs/common";
-import { DataRangeEntity, PaginationEntity, TransferEntity } from "src/data/persistence";
-import { NewTransferDTO } from "src/business/dtos";
-import { TransferService } from "src/business/services";
-import { JwtGuard } from "src/presentation/guards/guard-jwt";
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  DataRangeEntity,
+  PaginationEntity,
+  TransferEntity,
+} from 'src/data/persistence';
+import { NewTransferDTO } from 'src/business/dtos';
+import { TransferService } from 'src/business/services';
+import { JwtGuard } from 'src/presentation/guards/guard-jwt';
 
-@Controller("transfer")
+@Controller('transfer')
 export class TransferController {
-    constructor(private readonly transferService: TransferService) { }
+  constructor(private readonly transferService: TransferService) {}
 
-    @Get()
-    findAll() {
-        return this.transferService.findAll()
-    }
+  @Get()
+  findAll() {
+    return this.transferService.findAll();
+  }
 
-    @Get(":id")
-    //@UseGuards(JwtGuard)
-    findOneById(@Param("id", new ParseUUIDPipe()) id: string) {
-        return this.transferService.findOneById(id)
-    }
+  @Get(':id')
+  //@UseGuards(JwtGuard)
+  findOneById(@Param('id', new ParseUUIDPipe()) id: string) {
+    return this.transferService.findOneById(id);
+  }
 
-    @Post()
-    //@UseGuards(JwtGuard)
-    createTransfer(@Body() transfer: NewTransferDTO): TransferEntity {
-        return this.transferService.createTransfer(transfer)
-    }
+  @Post()
+  //@UseGuards(JwtGuard)
+  createTransfer(@Body() transfer: NewTransferDTO): TransferEntity {
+    return this.transferService.createTransfer(transfer);
+  }
 
-    @Post("/historyOut/:id")
-    //@UseGuards(JwtGuard)
-    getHistoryOut(@Param("id", new ParseUUIDPipe()) id: string, @Body() data: { actualPage: number, range: number }) {
-        const newPagination = new PaginationEntity()
-        newPagination.actualPage = data.actualPage;
-        const newDataRange = new DataRangeEntity()
-        newDataRange.range = data.range;
-        return this.transferService.getHistoryOut(id, newPagination, newDataRange)
-    }
+  @Post('/historyOut/:id')
+  //@UseGuards(JwtGuard)
+  getHistoryOut(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() data: { actualPage: number; range: number },
+  ) {
+    const newPagination = new PaginationEntity();
+    newPagination.actualPage = data.actualPage;
+    const newDataRange = new DataRangeEntity();
+    newDataRange.range = data.range;
+    return this.transferService.getHistoryOut(id, newPagination, newDataRange);
+  }
 
-    @Post("/historyIncome/:id")
-    //@UseGuards(JwtGuard)
-    getHistoryIncome(@Param("id", new ParseUUIDPipe()) id: string, @Body() data: { actualPage: number, range: number }) {
-        const newPagination = new PaginationEntity()
-        newPagination.actualPage = data.actualPage;
-        const newDataRange = new DataRangeEntity()
-        newDataRange.range = data.range;
-        return this.transferService.getHistoryIn(id, newPagination, newDataRange)
-    }
+  @Post('/historyIncome/:id')
+  //@UseGuards(JwtGuard)
+  getHistoryIncome(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() data: { actualPage: number; range: number },
+  ) {
+    const newPagination = new PaginationEntity();
+    newPagination.actualPage = data.actualPage;
+    const newDataRange = new DataRangeEntity();
+    newDataRange.range = data.range;
+    return this.transferService.getHistoryIn(id, newPagination, newDataRange);
+  }
 
-    @Post("/history/:id")
-    getHistory(@Param("id",new ParseUUIDPipe())id:string,@Body() data: { actualPage: number, range: number }){
-        const newPagination = new PaginationEntity()
-        newPagination.actualPage = data.actualPage;
-        const newDataRange = new DataRangeEntity()
-        newDataRange.range = data.range;
-        return this.transferService.getHistoryOutcomeCustomerId(id)
-    }
+  @Post('/history/:id')
+  getHistory(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() data: { actualPage: number; range: number },
+  ) {
+    const newPagination = new PaginationEntity();
+    newPagination.actualPage = data.actualPage;
+    const newDataRange = new DataRangeEntity();
+    newDataRange.range = data.range;
+    return this.transferService.getHistoryOutcomeCustomerId(id);
+  }
 }
